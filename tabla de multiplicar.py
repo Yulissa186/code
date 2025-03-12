d719775718994acba44e18406c360345f1d34a2a
Yulissa186 <yuli125blas@gmail.com>
2025-03-11 18:27:20 -0600

    Add files via upload

---
+++ tabla de multiplicar.py
@@ -0,0 +1,28 @@
+import os
+import time
+a=1
+
+while a <= 10:
+    
+    b=1
+    while b<=10:  
+       
+        print (f"{a} * {b} = {a*b}")
+        b += 1 
+    a += 1 
+    time.sleep(5)
+    os.system("cls")
+    print ()   
+     
+
+
+     
+
+
+    
+        
+
+
+    
+
+
