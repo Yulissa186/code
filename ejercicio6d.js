b40f77288650ae90b74e25b68c969b4f2a846487
Yulissa186 <yuli125blas@gmail.com>
2025-03-21 16:26:00 -0600

    Add files via upload

---
+++ ejercicio6d.js
@@ -0,0 +1,8 @@
+const fs = require('node:fs/promises')
+async function leerDirectorio() {
+ const archivos = await fs.readdir("./")
+ for (let archivo of archivos) {
+   console.log(archivo)
+ }
+}
+leerDirectorio()
