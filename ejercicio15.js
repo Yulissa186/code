af29ee109e8c812b8b3d4ff6dfc072e20ade208a
Yulissa186 <yuli125blas@gmail.com>
2025-05-07 21:30:14 -0600

    Add files via upload

---
+++ ejercicio15.js
@@ -0,0 +1,35 @@
+const http = require('node:http')
+const fs = require('node:fs')
+const mime = require('mime')
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'static' + url.pathname
+  if (camino == 'static/')
+    camino = 'static/index.html'
+  fs.stat(camino, error => {
+    if (!error) {
+      fs.readFile(camino, (error, contenido) => {
+        if (error) {
+          respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+          respuesta.write('Error interno')
+          respuesta.end()
+        } else {
+          const tipo = mime.getType(camino)
+          console.log(tipo)
+          respuesta.writeHead(200, { 'Content-Type': tipo })
+          respuesta.write(contenido)
+          respuesta.end()
+        }
+      })
+    } else {
+      respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+      respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+      respuesta.end()
+    }
+  })
+})
+
+servidor.listen(8888)
+
+console.log('Servidor web iniciado')
